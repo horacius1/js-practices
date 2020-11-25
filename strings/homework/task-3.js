32c116e0109418553c92cdde29914bb1ad662b56
user2 <oto.chechelashvili@gmail.com>
2020-11-25 19:06:33 +0400

    minor tweaks

--- strings/homework/task-3.js
+++ strings/homework/task-3.js
@@ -2,7 +2,10 @@ var searchWord = (str,word) => {
   let count=0
        
        var words=str.split(/([_\W])/)
-        
+        if (typeof str  !== 'string' && typeof word  !== 'string'){
+            throw new error('error: parameter is not a string')
+
+        }
         {
             for (i=0 ; i<words.length ;i++){
                 if (words[i]=== word)
@@ -13,5 +16,9 @@ var searchWord = (str,word) => {
         
     }
    
-
+try{
 console.log (searchWord('The quick brown fox', 'fox'))
+}
+catch (e){
+    console.log(e.massage)
+}
